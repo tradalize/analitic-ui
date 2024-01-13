1b86caf1072287d3100085b9a6e532eced8936f4
Vladyslav Bezsmertnyi <besmercj123@gmail.com>
2024-01-13 17:53:18 +0200

    Initial

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-layout>
+      <v-navigation-drawer>
+        <v-list nav>
+          <v-list-item title="Home" to="/" exact />
+          <v-list-item title="Backtests" :to="{ name: 'Backtests' }" />
+          <v-list-item
+            title="Compare backtests"
+            :to="{ name: 'CompareBacktests' }"
+          />
+          <v-list-item title="Models" :to="{ name: 'Models' }" />
+          <v-list-item title="Sandbox" :to="{ name: 'Sandbox' }" />
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+        <div class="pa-4 overflow-y-auto h-screen">
+          <nuxt-page />
+        </div>
+      </v-main>
+    </v-layout>
+  </v-app>
+</template>
---
+++ plugins/vuetify.ts
@@ -0,0 +1,15 @@
+import "@mdi/font/css/materialdesignicons.css";
+
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+
+export default defineNuxtPlugin((app) => {
+  const vuetify = createVuetify({
+    ssr: true,
+    theme: {
+      defaultTheme: "dark",
+    },
+  });
+
+  app.vueApp.use(vuetify);
+});
