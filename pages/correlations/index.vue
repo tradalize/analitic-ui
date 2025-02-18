30a7931de75334635d6c9c105a7e2f9d20ae8f43
Vladyslav Bezsmertnyi <besmerch123@gmail.com>
2025-02-18 21:39:57 +0200

    Correlations index page reworked

--- pages/correlations/index.vue
+++ pages/correlations/index.vue
@@ -19,14 +19,16 @@ const { data, status, refresh } = useFetch(
 </script>
 
 <template>
-  <div v-if="status === 'error'" class="flex flex-col items-center py-8">
-    <CrossCircledIcon class="h-12 w-12" />
+  <UContainer as="main">
+    <div v-if="status === 'error'" class="flex flex-col items-center py-8">
+      <CrossCircledIcon class="h-12 w-12" />
 
-    <h3 class="text-2xl font-bold my-8">
-      Looks like some error accured. Make sure that arbitrage API is up and
-      running
-    </h3>
-    <Button @click="refresh">Refresh</Button>
-  </div>
-  <CorrelationsTable v-else :data />
+      <h3 class="text-2xl font-bold my-8">
+        Looks like some error accured. Make sure that arbitrage API is up and
+        running
+      </h3>
+      <Button @click="refresh">Refresh</Button>
+    </div>
+    <CorrelationsTable v-else :data :loading="status === 'pending'" />
+  </UContainer>
 </template>
